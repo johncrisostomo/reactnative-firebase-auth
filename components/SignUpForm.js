ab2334a63979d73562188b680b9cb81a3d16e874
John Crisostomo <johncrisostomo@protonmail.ch>
2017-06-07 08:24:07 +0800

    implements createUser request

--- components/SignUpForm.js
+++ components/SignUpForm.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { FormLabel, FormInput, Button } from 'react-native-elements';
+import axios from 'axios';
+import { createAccount, requestOTP } from '../function_endpoints.json';
 
 class SignUpForm extends Component {
   state = { phone: '' };
 
-  handleSubmit = () => {
-
+  handleSubmit = async () => {
+    try {
+      await axios.post(createAccount, { phone: this.state.phone });
+      await axios.post(requestOTP, { phone: this.state.phone})
+    } catch (err) {
+      console.log(err);
+    }
   }
 
   render() {
